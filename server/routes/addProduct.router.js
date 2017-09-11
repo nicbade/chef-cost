bcfbfc4fa5ab7520bdf57b357e66cd2edb33ba47
Nic Bade <chipchapmaniii@gmail.com>
2017-09-11 17:29:20 -0500

    get route added and functional for addProduct route

--- server/routes/addProduct.router.js
+++ server/routes/addProduct.router.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var pool = require('../modules/pool.js');
 
+// addProduct post route
 router.post('/', function(req, res) {
     var newProduct = req.body;
     console.log('addProduct post was hit!');
@@ -21,11 +22,31 @@ router.post('/', function(req, res) {
                 } else {
                     res.sendStatus(201);
                 }
-            });
+            }); // end client.query
         }
-    });
-});
+    }); // end pool.connect
+}); // end post route
 
-
+router.get('/', function(req, res) {
+    pool.connect(function(err, db, done) {
+        if (err) {
+            console.log('Error connecting to the DB', err);
+            res.sendStatus(500);
+            done();
+            return;
+        } // end error
+        else {
+            db.query('SELECT * FROM product', function(errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    console.log('Error making database query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                } // end if statement
+            });
+        } // end no error
+    }); // end pool connect
+}); // end get 
 
 module.exports = router;
