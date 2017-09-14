aa9dcff0eb60b9ecaa2bcfc6c7632898eb7c3134
Nic Bade <chipchapmaniii@gmail.com>
2017-09-13 19:44:30 -0500

    fixed bug with drop down menus

--- server/public/scripts/services/recipe.service.js
+++ server/public/scripts/services/recipe.service.js
@@ -1,12 +1,20 @@
 myApp.service('RecipeService', ['$http', function($http) {
     console.log('RecipeService loaded');
     var self = this;
+    self.recipe = { list: [] };
 
     self.addRecipe = function(newRecipe) {
         console.log(newRecipe);
         $http.post('/addRecipe', newRecipe).then(function(response) {
             console.log('service post reponse: ', response);
             self.getRecipe();
+        });
+    };
+
+    self.getRecipe = function() {
+        $http.get('/addRecipe').then(function(response) {
+            console.log('getroute addRecipe: ', response.data);
+            self.recipe.list = response.data;
         });
     };
 }]);
--- server/routes/addRecipe.router.js
+++ server/routes/addRecipe.router.js
@@ -6,7 +6,6 @@ var pool = require('../modules/pool.js');
 router.post('/', function(req, res) {
     var newRecipe = req.body;
     console.log('addProduct post was hit!');
-    // Add an INSERT query
     pool.connect(function(errorConnectingToDatabase, client, done) {
         if (errorConnectingToDatabase) {
             //when connecting to database failed
@@ -14,7 +13,7 @@ router.post('/', function(req, res) {
             res.sendStatus(500);
         } else {
             // when connecting to database worked aka HAPPYPATH!
-            client.query('INSERT INTO recipe (name, type, yield, yield_amount) VALUES ($1, $2, $3, $4;', [newRecipe.name, newRecipe.type, newRecipe.yield, newRecipe.yield_amount], function(errorMakingQuery, result) {
+            client.query('INSERT INTO recipe (name, type, yield, yield_amount) VALUES ($1, $2, $3, $4);', [newRecipe.name, newRecipe.type, newRecipe.yield, newRecipe.yield_amount], function(errorMakingQuery, result) {
                 done(); //needed
                 if (errorMakingQuery) {
                     console.log('Error making database query', errorMakingQuery);
@@ -26,5 +25,27 @@ router.post('/', function(req, res) {
         }
     }); // end pool.connect
 }); // end post route
+
+router.get('/', function(req, res) {
+    pool.connect(function(err, db, done) {
+        if (err) {
+            console.log('Error connecting to the DB', err);
+            res.sendStatus(500);
+            done();
+            return;
+        } // end error
+        else {
+            db.query('SELECT * FROM recipe', function(errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    console.log('Error making database query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                } // end if statement
+            });
+        } // end no error
+    }); // end pool connect
+}); // end get
 
 module.exports = router;
